91053345b3a224bd7d5ed4c12ceceecf172ce1b7
Vinicius Mayrink <vmayrink12@gmail.com>
2026-03-01 16:18:45 -0300

    feat(bin/gnome-focus-recent-window): now expects steps as `--steps` and supports modes for only same application instances and only other applications
    The default steps is now 1.
    
    The keybindings changed: `<Super>0` switch to last instance, `<Super>9`
    to the last application and `<Super>9` the last non-favorite.

--- extensions/flake.nix
+++ extensions/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Pure Flake GNOME Extensions Management";
+  description = "GNOME Extensions Management";
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
